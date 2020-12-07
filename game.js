202d1bc9cc3797d8d26a5c39001586b7061edd54
Prince Csaba <bolyaicsaba@gmail.com>
2020-12-07 12:50:40 +0100

    monday work

--- game.js
+++ game.js
@@ -1,7 +1,35 @@
+function dropDown () {
+  let x = document.querySelector(".gift").style.top;
+  let strX = x.replace("px", "");
+  console.log(strX);
+  let numX = parseInt(strX);
+  document.querySelector(".gift").style.top = `${numX+10}px`;
+  //console.log(document.querySelector(".gift").style.top);
+}
+
+function onLoad () {
+
+  const root = document.getElementById("root");
+  
+  root.insertAdjacentHTML("beforeend", "<div class='gift'></div>");
+  let i = 20;
+  dropDown();
+  setInterval(dropDown, 500);
+
+}
+
+window.addEventListener("load", onLoad);
+
+
+
+
+
+
+/* 
 initGame();
 
 function initGame() {
 
   // Your game can start here, but define separate functions, don't write everything in here :)
-
 }
+ */
