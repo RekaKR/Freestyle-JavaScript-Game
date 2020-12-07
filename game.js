c6d3aefef8b60da8b865cf7c1e8f9b12783b745c
Prince Csaba <bolyaicsaba@gmail.com>
2020-12-08 00:30:24 +0100

    basics donne

--- game.js
+++ game.js
@@ -1,24 +1,44 @@
-function dropDown () {
-  let x = document.querySelector(".gift").style.top;
-  let strX = x.replace("px", "");
+function dropDown (i) {
+  console.log(i);
+  let p = i.toString()+"px";
+  console.log(p);
+  document.querySelector(".gift").style.top = p;
+  i += 10;
+  return i;
+  /* let strX = x.replace("px", "");
   console.log(strX);
   let numX = parseInt(strX);
   document.querySelector(".gift").style.top = `${numX+10}px`;
-  //console.log(document.querySelector(".gift").style.top);
+  *///console.log(document.querySelector(".gift").style.top);
 }
 
-function onLoad () {
+function remove(){
+  root.querySelector(".gift").classList.add("removed");
+}
 
+function color(){
+  root.querySelector(".gift").classList.add("color");
+}
+
+
+function onLoad () {
+  
   const root = document.getElementById("root");
+  let highScore = 0;
   
   root.insertAdjacentHTML("beforeend", "<div class='gift'></div>");
-  let i = 20;
-  dropDown();
-  setInterval(dropDown, 500);
-
+  
+  i = 0;
+  setInterval( function () { i = dropDown(i)}, 1000);
+  
+  
+  root.addEventListener("click", remove);
+  highScore += 1;
+  console.log(highScore);
 }
 
 window.addEventListener("load", onLoad);
+
 
 
 
