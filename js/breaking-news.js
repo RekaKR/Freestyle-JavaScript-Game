a5d3f650f6d2634afc2d853bc817cd7c3773fbe3
lattila1 <73441214+lattila1@users.noreply.github.com>
2020-12-10 21:50:47 +0100

    Fix breaking news, remove unused files

--- js/breaking-news.js
+++ js/breaking-news.js
@@ -1,12 +1,38 @@
 window.addEventListener('load', () => {
-  document.querySelector('body').addEventListener('click', () => {
-    let xmasSound = document.getElementById('xmas-sound');
-    xmasSound.addEventListener('ended', () => {
-      document.querySelector('.drama').classList.toggle('hide');
-      document.querySelector('.xmas').classList.toggle('hide');
-      let dramaSound = document.getElementById('drama-sound');
-      dramaSound.play();
-    });
+  document.getElementById('start-intro').addEventListener('click', startIntro);
+  let xmasSound = document.getElementById('xmas-sound');
+  
+  function startIntro(e) {
+    e.target.remove();
+    xmasSound.volume = 0.1;
     xmasSound.play();
-  });
+    setTimeout(() => {
+      xmasSound.pause();
+      startDrama();      
+    }, 5000);
+  }
+
+  function startDrama() {
+    document.querySelector('.drama').classList.remove('hide');
+    document.querySelector('.xmas').classList.add('hide');
+
+    let dramaSound = document.getElementById('drama-sound');
+    dramaSound.volume = 0.1;
+    dramaSound.play();
+
+    setTimeout(() => {
+      document.querySelector('.newspaper img').remove();
+      document.querySelector('.newspaper').insertAdjacentHTML('beforeend', '<img src="img/newspaper2.jpg" alt="">');
+    }, 5000);
+
+    setTimeout(() => {
+      document.querySelector('.newspaper img').remove();
+      document.querySelector('.newspaper').insertAdjacentHTML('beforeend', '<img src="img/newspaper3.jpg" alt="">');
+    }, 10000);
+
+    setTimeout(() => {
+      document.querySelector('.drama').classList.add('hide');
+      document.querySelector('.instructions').classList.remove('hide');
+    }, 15000);
+  }
 });
